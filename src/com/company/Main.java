4773ac51ab401ab35956bc616e02ac338925547a
vijay <bandivijay.vk@gmail.com>
2021-08-29 22:02:37 +0530

    new change3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("welcome to intelj");
-
+        System.out.println("welcome to intelj");
+        System.out.println("welcome to intelj");
+        System.out.println("welcome to intelj");
+        System.out.println("welcome to intelj");
         System.out.println("welcome to intelj");
     }
 }
